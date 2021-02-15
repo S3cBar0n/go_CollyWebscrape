954c0f489dbdd63575bc8ca107bd5fe4762723aa
S3cBar0n <secberg@gmail.com>
2021-02-15 11:40:27 -0700

    Basic formatting changes, removing whitespace.

--- main.go
+++ main.go
@@ -8,49 +8,10 @@ import (
 	"log"
 )
 
-
 type news struct {
-	Subject string `csv:"home-title"`
-	Description string `csv:"home-desc"`
-	Link string `csv:"story-link"`
-}
-
-
-func main() {
-	allNews := make([]news, 0)
-
-	collector := colly.NewCollector(
-		colly.AllowedDomains("thehackernews.com"), // Add an Array made from CSV that has multiple domains.
-		)
-
-	collector.OnHTML(".body-post", func(element *colly.HTMLElement) {
-		newsSubject := element.ChildText("h2.home-title")
-		newsDescription := element.ChildText("div.home-desc")
-		newsLink := element.ChildAttr("a", "href")
-
-		news := news{
-			Subject: newsSubject,
-			Description: newsDescription,
-			Link: newsLink,
-		}
-
-		allNews = append(allNews, news)
-	})
-
-	collector.OnRequest(func(request *colly.Request) {
-		fmt.Println("Visiting", request.URL.String())
-	})
-
-	collector.Visit("https://thehackernews.com/")
-
-	writeJSON(allNews)
-
-
-// Todo Add domains via CSV to Array Variable
-// Todo Add switch function to switch between multiple domains
-// Todo Change from exporting to JSON to CSV
-
-
+	Subject     string `json:"home-title"`
+	Description string `json:"home-desc"`
+	Link        string `json:"story-link"`
 }
 
 func writeJSON(data []news) {
@@ -59,7 +20,38 @@ func writeJSON(data []news) {
 		log.Println("Unable to create JSON file")
 		return
 	}
-
 	_ = ioutil.WriteFile("news.json", file, 0644)
+}
 
+func main() {
+	allNews := make([]news, 0)
+
+	collector := colly.NewCollector(
+		colly.AllowedDomains("thehackernews.com"), // Add an Array made from CSV that has multiple domains.
+	)
+	collector.OnHTML(".body-post", func(element *colly.HTMLElement) {
+		newsSubject := element.ChildText("h2.home-title")
+		newsDescription := element.ChildText("div.home-desc")
+		newsLink := element.ChildAttr("a", "href")
+		news := news{
+			Subject:     newsSubject,
+			Description: newsDescription,
+			Link:        newsLink,
+		}
+		allNews = append(allNews, news)
+	})
+
+	collector.OnRequest(func(request *colly.Request) {
+		fmt.Println("Visiting", request.URL.String())
+	})
+	err := collector.Visit("https://thehackernews.com/")
+	if err != nil {
+		log.Println("Could not visit the listed website.")
+	}
+
+	writeJSON(allNews)
+
+	// Todo Add domains via CSV to Array Variable
+	// Todo Add switch function to switch between multiple domains
+	// Todo Change from exporting to JSON to CSV
 }
